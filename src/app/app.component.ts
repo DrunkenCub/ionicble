8ec8128b6594d3cd4670913cfa196b271c03f39c
drunkencub <chathuranga@triolly.com>
2020-05-04 10:06:48 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,6 +3,7 @@ import { Component } from '@angular/core';
 import { Platform } from '@ionic/angular';
 import { SplashScreen } from '@ionic-native/splash-screen/ngx';
 import { StatusBar } from '@ionic-native/status-bar/ngx';
+import { BluetoothLE } from '@ionic-native/bluetooth-le/ngx';
 
 @Component({
   selector: 'app-root',
@@ -13,13 +14,42 @@ export class AppComponent {
   constructor(
     private platform: Platform,
     private splashScreen: SplashScreen,
-    private statusBar: StatusBar
+    private statusBar: StatusBar,
+    private bluetoothle: BluetoothLE
   ) {
     this.initializeApp();
   }
 
   initializeApp() {
     this.platform.ready().then(() => {
+      const params = {
+        request: true,
+        statusReceiver: true,
+        restoreKey : 'bluetoothleplugin'
+      };
+
+      this.bluetoothle.initialize(params).subscribe(ble => {
+        console.log('ble', ble.status); // logs 'enabled'
+      });
+
+      const scanParams = {
+        services: [
+          '180D',
+          '180F'
+        ],
+        allowDuplicates: true,
+        scanMode: this.bluetoothle.SCAN_MODE_LOW_LATENCY,
+        matchMode: this.bluetoothle.MATCH_MODE_AGGRESSIVE,
+        matchNum: this.bluetoothle.MATCH_NUM_MAX_ADVERTISEMENT,
+        callbackType: this.bluetoothle.CALLBACK_TYPE_ALL_MATCHES,
+      };
+
+      this.bluetoothle.startScan(scanParams).subscribe((status) => {
+        alert(status);
+      }, () => {
+
+      });
+
       this.statusBar.styleDefault();
       this.splashScreen.hide();
     });
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,12 +8,14 @@ import { StatusBar } from '@ionic-native/status-bar/ngx';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
+import { BluetoothLE } from '@ionic-native/bluetooth-le/ngx';
 
 @NgModule({
   declarations: [AppComponent],
   entryComponents: [],
   imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
   providers: [
+    BluetoothLE,
     StatusBar,
     SplashScreen,
     { provide: RouteReuseStrategy, useClass: IonicRouteStrategy }
